ec12c54a3ff3d60afd29c291b092ddb85c80fa21
Mickey <shmumic@post.bgu.ac.il>
2019-10-04 16:03:20 +0300

    Models of servers entities.

--- models/grat.model.js
+++ models/grat.model.js
@@ -1,1 +1,49 @@
-""
+const mongoose = require("mongoose");
+const gratSchema = new mongoose.Schema({ //grat stands for gratitude
+
+  gratText: { //the actual gratitude
+    type: String, //validate length
+    minlength: 2,
+    maxlength: 280 //just like nowdays twitter
+  },
+
+  rating: {
+    type: Number
+  }, //user ratings a gratitude with a low rating will be removed.
+  isPublic: {
+    type: Boolean,
+    required: true
+  }, //is the gratitude for the the eyes of everyone to see
+  published: {
+    type: Boolean
+  },
+  source:{
+    userId : String,//links to the author
+    ip:String,
+    browser: String,
+    location: { //GeoJson
+      type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ['Point'], // 'location.type' must be 'Point'
+        required: true
+      },
+      coordinates: {
+        type: [Number],
+        required: true
+      }
+    }
+  },
+  date: {
+
+  },
+  expiretionDate: {},
+  hashtags: {
+    type: String
+  },
+  categories: {
+    type: String
+  }
+
+});
+module.exports.Grat = mongoose.model("Grat", gratSchema);
+module.export.gratSchema = gratSchema;
--- models/user.model.js
+++ models/user.model.js
@@ -1,1 +1,25 @@
-""
+const mongoose = require("mongoose");
+import gratSchema from require("./grat.model");
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50
+  },
+  AuthanticationMethod: { ///TBD
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  gender: {
+    type: String,
+    required: true
+  }
+  gratitudes: [gratSchema]
+
+});
+
+module.exports.User = mongoose.model("User", userSchema);
